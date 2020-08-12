1d7019157e444d4136380b90c81cba7bca674645
hanoelleb <36749694+hanoelleb@users.noreply.github.com>
2020-08-12 00:12:14 -0700

    cell finding buggy

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,6 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
